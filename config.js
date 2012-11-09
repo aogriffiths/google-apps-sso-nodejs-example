045cec51a8ecf2c247e07ff4d68aff1bb1a0fe17
Adam Griffiths <aogriffiths@gmail.com>
2012-11-09 15:24:21 +0000

    updates

--- config.js
+++ config.js
@@ -3,12 +3,14 @@ var config = {};
 config.defs = {
   localhost: {
     name:  'localhost',
-    domain:'<your_google_apps_domain>',
+    domain:'tendogs.net',
     realm: 'http://localhost:5000'
   },
   production: {
     name:  'production',
-    domain:'<your_google_apps_domain>',
-    realm: 'http://<your_production_app_domain>'
+    domain:'tendogs.net',
+    realm: 'http://still-springs-4775.herokuapp.com/'
   }
-};;;
+};
+
+module.exports = config;
--- web.js
+++ web.js
@@ -40,16 +40,6 @@ passport.deserializeUser(function(obj, done) {
 done(null, obj);
 });
 
-
-//configure passport strategies from the config file
-var strategy_defs = config.defs;
-
-for(key in strategy_defs){
-  def = strategy_defs[key];
-  def.strategy = createStrategy(def);
-  passport.use(def.strategy);
-}
-
 var createStrategy = function(def){
   var strategy = new GoogleStrategy({
     returnURL:   def.realm + '/auth/return/' + def.name,
@@ -66,6 +56,18 @@ var createStrategy = function(def){
   strategy.name = def.name;
   return strategy;
 }
+
+//configure passport strategies from the config file
+util.debug("Loading strategies");
+var strategy_defs = config.defs;
+
+for(key in strategy_defs){
+  def = strategy_defs[key];
+  util.debug("Loading strategy: " + def.name);
+  def.strategy = createStrategy(def);
+  passport.use(def.strategy);
+}
+
 
 
 
