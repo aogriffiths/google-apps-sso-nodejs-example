2cbb90d0ec0dc6a26f38391f6111bc1308e5a7d0
Adam Griffiths <aogriffiths@gmail.com>
2012-11-09 18:40:04 +0000

    updated with newsint

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ config.defs = {
     domain:'tendogs.net',
     realm: 'http://localhost:5000'
   },
-  production: {
-    name:  'production tendogs',
+  productiontd: {
+    name:  'productiontendogs',
     domain:'tendogs.net',
     realm: 'http://still-springs-4775.herokuapp.com/'
-  }
-  production: {
-    name:  'production newsint',
+  },
+  productionni: {
+    name:  'productionnewsint',
     domain:'newsint.co.uk',
     realm: 'http://still-springs-4775.herokuapp.com/'
   }
